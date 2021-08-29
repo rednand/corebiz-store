73aac43e92b69f5dad7ea4313fa6993d409698d9
Douglas Figueiredo <douglasarts@gmail.com>
2021-08-28 21:14:34 -0300

    Formulario para cadastro de Clientes

--- react/Clients/config/index.ts
+++ react/Clients/config/index.ts
@@ -1,2 +1,3 @@
-export const BASE_URL =  'https://mdz06pnka0.execute-api.us-east-2.amazonaws.com'
+export const BASE_URL =  'https://n3me17g086.execute-api.us-east-2.amazonaws.com'
+// export const BASE_URL =  'https://mdz06pnka0.execute-api.us-east-2.amazonaws.com'
 
--- react/FormLead.tsx
+++ react/FormLead.tsx
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import http from './Clients/AWSClient'
-import { Container, Card, Title, Info } from './styles/clientes'
+import { Button, Container, Input, Error, Success } from './styles/formulario'
+
 interface Lead {
   id: string;
   nome: string;
@@ -8,31 +9,105 @@ interface Lead {
   email: string;
   prospecto: boolean;
 }
- 
+
 const FormLead: StorefrontFunctionComponent = () =>  {
-  const [leads, setLeads] = useState<Lead[]>([])
-  useEffect(() => {
-    http
-      .get('/users')
-      .then((response) => response.data)
-      .then((data) => setLeads(data["Items"])
-      )
-    }, [])
+  
+  let leadObj: Lead;
+  const [error, setError] = useState(false)
+  const [sendSuccess, setSendSuccess] = useState(false)
+  const [lead, setLead] = useState({
+    nome: "",
+    sobrenome: "",
+    email: "",
+    prospecto: null,
+  });
+  
+  const handleCreateNewLead = (property: string, value: string) => {
+    setLead((prev) => ({ ...prev, [property]: value }));
+  }
+
+  const handleSubmit = (e: React.SyntheticEvent) => {
+    setError(false);
+    e.preventDefault();
+    if (!lead.email) {
+      setError(true);
+      return;
+    }
+    
+    leadObj = {
+      prospecto: false,
+      nome: lead.nome,
+      id: '9',
+      email: lead.email,
+      sobrenome: lead.sobrenome,
+    };
+    sendLead(leadObj);
+  };
+  
+  const sendLead =  (lead: Lead) =>  {
+    console.log(lead);
+     http
+      .put('/users', lead).then((res) => {
+        console.log(res)
+        setLead({ nome: "", sobrenome: "", email: "", prospecto: null });
+        setSendSuccess(true);
+      })
+  };
 
   return(
-    <Container>
+    <>
       { 
-        leads.map((lead)=>{
-          if(lead.prospecto==true){
-            return(<Card>
-              <Title>{lead.nome}</Title>
-              <Info>Cliente desde: <span>2020</span></Info>
-            </Card>)
-          }
-          return
-        })
+        sendSuccess?
+        <Container>
+          <Success>Cadastro efetuado com sucesso!</Success>
+          <Button type="button" onClick={()=>setSendSuccess(false)}>Voltar</Button>
+        </Container>
+        :
+        <Container>
+          <Input
+            type="text"
+            placeholder="Nome"
+            value={lead.nome}
+            required
+            onChange={({
+              target: { value },
+            }: React.ChangeEvent<HTMLInputElement>) =>
+            handleCreateNewLead("nome", value)
+            }
+          />
+          
+          <Input
+            type="text"
+            placeholder="Sobrenome"
+            value={lead.sobrenome}
+            required
+            onChange={({
+              target: { value },
+            }: React.ChangeEvent<HTMLInputElement>) =>
+            handleCreateNewLead("sobrenome", value)
+            }
+          />
+          <Input
+            type="text"
+            placeholder="E-mail"
+            value={lead.email}
+            required
+            onChange={({
+              target: { value },
+            }: React.ChangeEvent<HTMLInputElement>) =>
+            handleCreateNewLead("email", value)
+            }
+          />
+          <Button
+            type="submit"
+            onClick={handleSubmit}
+            >
+            Enviar
+          </Button>
+          {error ?<Error>Todos o campos são obrigatório</Error>:<div></div>}
+        </Container>
       }
-    </Container>
+    </>
   )
 }
 
